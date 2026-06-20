413860ba710ac557b709cc9da6ad9805c29afb65
Guido Giglioli <guido.tobugroup@gmail.com>
2026-06-20 22:34:47 +0200

    aggiunta logica popolazione cards

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
 const teamMembers = [
-  {
-    name: "Marco Bianchi",
-    role: "Designer",
-    email: "marcobianchi@team.com",
-    img: "assets/img/male1.png"
-  },
-  {
-    name: "Laura Rossi",
-    role: "Front-end Developer",
-    email: "laurarossi@team.com",
-    img: "assets/img/female1.png"
-  },
-  {
-    name: "Giorgio Verdi",
-    role: "Back-end Developer",
-    email: "giorgioverdi@team.com",
-    img: "assets/img/male2.png"
-  },
-  {
-    name: "Marta Ipsum",
-    role: "SEO Specialist",
-    email: "martarossi@team.com",
-    img: "assets/img/female2.png"
-  },
-  {
-    name: "Roberto Lorem",
-    role: "SEO Specialist",
-    email: "robertolorem@team.com",
-    img: "assets/img/male3.png"
-  },
-  {
-    name: "Daniela Amet",
-    role: "Analyst",
-    email: "danielaamet@team.com",
-    img: "assets/img/female3.png"
-  }
+    {
+        name: "Marco Bianchi",
+        role: "Designer",
+        email: "marcobianchi@team.com",
+        img: "assets/img/male1.png",
+    },
+    {
+        name: "Laura Rossi",
+        role: "Front-end Developer",
+        email: "laurarossi@team.com",
+        img: "assets/img/female1.png",
+    },
+    {
+        name: "Giorgio Verdi",
+        role: "Back-end Developer",
+        email: "giorgioverdi@team.com",
+        img: "assets/img/male2.png",
+    },
+    {
+        name: "Marta Ipsum",
+        role: "SEO Specialist",
+        email: "martarossi@team.com",
+        img: "assets/img/female2.png",
+    },
+    {
+        name: "Roberto Lorem",
+        role: "SEO Specialist",
+        email: "robertolorem@team.com",
+        img: "assets/img/male3.png",
+    },
+    {
+        name: "Daniela Amet",
+        role: "Analyst",
+        email: "danielaamet@team.com",
+        img: "assets/img/female3.png",
+    },
 ];
-
 
 /* const teamListUl = document.getElementById('team-list');
 
@@ -49,3 +48,34 @@ for (const member of teamMembers) {
     <p>Mail: ${member.email}</p>
   </li>`;
 } */
+
+const tmplMemberCard = (member) => {
+  const {name, role, email, img} = member;
+  return `
+  <div class="team-card">
+      <div class="card-image">
+          <img src="${img}" alt="${name}" />
+      </div>
+      <div class="card-text">
+          <h3>${name}</h3>
+          <p>${role}</p>
+          <p>${email}</p>
+      </div>
+  </div>
+  `;
+}
+  
+
+const teamContainerEl = document.querySelector('.team-container');
+
+const renderTeam = teamMembers => {
+  for (const member of teamMembers) {
+    teamContainerEl.innerHTML += tmplMemberCard(member);
+  }  
+}
+
+renderTeam(teamMembers);
+
+
+
+
