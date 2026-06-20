ee1262ce5a017c35125904c58863a19d4be8cf68
Guido Giglioli <guido.tobugroup@gmail.com>
2026-06-20 15:44:55 +0200

    test inserimento dati in DOM tramite JS

--- script.js
+++ script.js
@@ -3,36 +3,49 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "assets/img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "assets/img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "assets/img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "assets/img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "assets/img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "assets/img/female3.png"
   }
 ];
+
+
+const teamListUl = document.getElementById('team-list');
+
+for (const member of teamMembers) {
+  teamListUl.innerHTML += `
+  <li>
+    <img src="${member.img}"></img>
+    <p>Name: ${member.name}</p>
+    <p>Role: ${member.role}</p>
+    <p>Mail: ${member.email}</p>
+  </li>`;
+}
